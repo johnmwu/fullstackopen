17d9e1e30ed0283fc1563b2229cba2d6a5302e2e
John Michael Wu <johnmichaelwu@johns-air.mynetworksettings.com>
2024-06-20 18:27:30 -0400

    2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -9,6 +9,11 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
 
+    if (persons.map(person => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
     const personObject = {
       name: newName
     }
