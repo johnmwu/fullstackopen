ca22b750f4ffbfcfaa52e8ee6c4ab7d513bddac4
John Michael Wu <johnmichaelwu@gmail.com>
2024-07-04 14:27:49 -0400

    3.11

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 app.use(morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
@@ -37,10 +40,6 @@ let persons = [
     "number": "39-23-6423122"
   }
 ]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
@@ -96,7 +95,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
